899d0ce47b2a0727b6d67b06f93c9ad43c4bc514
Suprith461 <59663680+Suprith461@users.noreply.github.com>
2021-11-19 22:51:35 +0530

    Added readme.md file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZWWK-NppFUefV71CitgcyR_JlVNqbydQ",
+  authDomain: "urlshortner-65dd0.firebaseapp.com",
+  projectId: "urlshortner-65dd0",
+  storageBucket: "urlshortner-65dd0.appspot.com",
+  messagingSenderId: "1011945942150",
+  appId: "1:1011945942150:web:1714e1a8437e747327f507",
+  measurementId: "G-7BKNZ77T7K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import express from 'express'
+
+const app = express();
+
+// Connect to database
+
+
+app.use(express.json());
+
+// Define Routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 ReactDOM.render(
   <React.StrictMode>

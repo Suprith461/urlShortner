2de6e5a59e5f2f818a77fba48a9e79faef1029e1
Suprith461 <59663680+Suprith461@users.noreply.github.com>
2021-11-21 15:32:43 +0530

    many new changes

--- src/api/models/generateHash.js
+++
@@ -1,6 +0,0 @@
-import crypto from 'crypto';
-
-export default function generateShortUrl(longUrl){
-    const hash = crypto.createHash("md5").update(longUrl).digest('hex')
-    return hash.toString().slice(0,6)
-}
